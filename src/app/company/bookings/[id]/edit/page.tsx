c8a6df767e45d6ab74cdbbba16c09c6a33c37cff
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-09-17 20:00:22 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/company/bookings/[id]/edit/page.tsx
+++ src/app/company/bookings/[id]/edit/page.tsx
@@ -4,10 +4,10 @@
 import NewBookingPage from '@/app/company/bookings/new/page';
 import { Suspense } from 'react';
 
-export default function EditBookingRootPage({ params }: { params: { id: string } }) {
+export default function EditBookingRootPage({ params: { id } }: { params: { id: string } }) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <NewBookingPage bookingId={params.id} />
+      <NewBookingPage bookingId={id} />
     </Suspense>
   );
 }
