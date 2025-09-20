46770500c29336252e15fbcb739832db7c9b768e
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-09-20 07:24:01 +0000

    As comment in the code notes that this page is intended as the default v

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
 
+// This page is no longer used. The login form is now at the root path '/'.
 export default function LoginPage() {
-  redirect('/');
+  return null;
 }
