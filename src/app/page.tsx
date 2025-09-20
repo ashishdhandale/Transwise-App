df41d048cdbcf8f6c0834299b78115d637be602c
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-09-20 07:22:24 +0000

    mian entru poiint of the application is login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-'use client';
-
-import { Suspense } from 'react';
-import DashboardLayout from './(dashboard)/layout';
-import DashboardPage from './(dashboard)/page';
-
-// This is the default page, which we'll use for the "Branch" user role.
-function App() {
-  return (
-    <DashboardLayout>
-      <DashboardPage />
-    </DashboardLayout>
-  );
-}
+import LoginForm from '@/components/auth/login-form';
 
 export default function RootPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
-  );
+  return <LoginForm />;
 }
