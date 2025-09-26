eb4a4efff29d99e02d8b893f89bf8d7a2d80fb3c
Abhijeet Sambaray <abhijeetsambaray@gmail.com>
2025-09-26 12:49:40 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/company/bookings/item-details-table.tsx
+++ src/components/company/bookings/item-details-table.tsx
@@ -24,6 +24,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { cn } from '@/lib/utils';
