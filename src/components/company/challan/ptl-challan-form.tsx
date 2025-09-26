ba398a1f47ec2a3178e17502333ec93e1159a3d5
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-09-26 05:02:00 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/company/challan/ptl-challan-form.tsx
+++ src/components/company/challan/ptl-challan-form.tsx
@@ -670,7 +670,7 @@ export function PtlChallanForm() {
     if (isLoading) {
         return <div className="flex justify-center items-center h-64"><Loader2 className="h-8 w-8 animate-spin" /></div>;
     }
-    
+
     return (
         <div className="space-y-2">
             
