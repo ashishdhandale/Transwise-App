f14f6a128be801b42b9117dd14745c67fba9d58e
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-10-11 07:16:39 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/company/challan/page.tsx
+++ src/app/company/challan/page.tsx
@@ -3,11 +3,14 @@
 import { Suspense } from 'react';
 import DashboardLayout from '../../(dashboard)/layout';
 import { ChallanDashboard } from '@/components/company/challan/challan-dashboard';
+import { ClientOnly } from '@/components/ui/client-only';
 
 function ChallanPage() {
   return (
     <DashboardLayout>
-      <ChallanDashboard />
+      <ClientOnly>
+        <ChallanDashboard />
+      </ClientOnly>
     </DashboardLayout>
   );
 }
