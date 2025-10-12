62d132b47a5f5c8218fbbb5ed911dd4fbcd8342c
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-10-12 20:30:41 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/company/master/vehicle-management.tsx
+++ src/components/company/master/vehicle-management.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
 
