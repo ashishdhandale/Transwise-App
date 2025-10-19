8b8768ee8c05da901833bd770c73bcb015a96238
Ashish Dhandale <Ashish.dhandale@gmail.com>
2025-10-19 07:56:08 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/company/deliveries/bulk/page.tsx
+++ src/app/company/deliveries/bulk/page.tsx
@@ -4,13 +4,16 @@
 import { Suspense } from 'react';
 import DashboardLayout from '../../../(dashboard)/layout';
 import { BulkDeliveryForm } from '@/components/company/deliveries/bulk-delivery-form';
+import { ClientOnly } from '@/components/ui/client-only';
 
 
 function BulkDeliveryPage() {
   return (
     <DashboardLayout>
       <main className="flex-1 p-4 md:p-6">
-        <BulkDeliveryForm />
+        <ClientOnly>
+          <BulkDeliveryForm />
+        </ClientOnly>
       </main>
     </DashboardLayout>
   );
